f85033f07a79d08c53aa32f8fec0556bc329769f
algokelvin-373 <kelvinht373@gmail.com>
2022-01-05 16:40:42 +0700

    [1.0.0-alpha] Create class 'PinEntry' and success to implement

--- app/src/main/java/com/algokelvin/pinentryedittext/MainActivity.kt
+++ app/src/main/java/com/algokelvin/pinentryedittext/MainActivity.kt
@@ -1,94 +1,29 @@
 package com.algokelvin.pinentryedittext
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.algokelvin.pinentryedittext.databinding.ActivityMainBinding
-import com.algokelvin.pinentryedittext.databinding.IncludePinBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var includePinBinding: IncludePinBinding
+    private lateinit var pinEntry: PinEntry
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        includePinBinding = binding.includePinEntry
 
-        includePinBinding.edtPin1.transformationMethod = AsteriskPasswordTransformationMethod()
-        includePinBinding.edtPin2.transformationMethod = AsteriskPasswordTransformationMethod()
-        includePinBinding.edtPin3.transformationMethod = AsteriskPasswordTransformationMethod()
-        includePinBinding.edtPin4.transformationMethod = AsteriskPasswordTransformationMethod()
-        includePinBinding.edtPin5.transformationMethod = AsteriskPasswordTransformationMethod()
-        includePinBinding.edtPin6.transformationMethod = AsteriskPasswordTransformationMethod()
+        // Implement PinEntry
+        pinEntry = PinEntry(binding.includePinEntry)
 
-        includePinBinding.edtPin1.addTextChangedListener(inputPinController(includePinBinding.edtPin1))
-        includePinBinding.edtPin2.addTextChangedListener(inputPinController(includePinBinding.edtPin2))
-        includePinBinding.edtPin3.addTextChangedListener(inputPinController(includePinBinding.edtPin3))
-        includePinBinding.edtPin4.addTextChangedListener(inputPinController(includePinBinding.edtPin4))
-        includePinBinding.edtPin5.addTextChangedListener(inputPinController(includePinBinding.edtPin5))
-        includePinBinding.edtPin6.addTextChangedListener(inputPinController(includePinBinding.edtPin6))
+        binding.apply {
+            bindingApply()
+        }
     }
 
-    private fun inputPinController(view: View) = object : TextWatcher {
-        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
-        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            val numberOne = includePinBinding.edtPin1.text.toString().trim()
-            val numberTwo = includePinBinding.edtPin2.text.toString().trim()
-            val numberThree = includePinBinding.edtPin3.text.toString().trim()
-            val numberFour = includePinBinding.edtPin4.text.toString().trim()
-            val numberFive = includePinBinding.edtPin5.text.toString().trim()
-            val numberSix = includePinBinding.edtPin6.text.toString().trim()
-
-            when(view.id) {
-                R.id.edt_pin_1 -> {
-                    if (numberOne.length == 1) {
-                        includePinBinding.edtPin2.requestFocus()
-                    }
-                }
-                R.id.edt_pin_2 -> {
-                    if (numberTwo.length == 1) {
-                        includePinBinding.edtPin3.requestFocus()
-                    } else if (numberTwo.isEmpty()) {
-                        includePinBinding.edtPin1.requestFocus()
-                    }
-                }
-                R.id.edt_pin_3 -> {
-                    if (numberThree.length == 1) {
-                        includePinBinding.edtPin4.requestFocus()
-                    } else if (numberThree.isEmpty()) {
-                        includePinBinding.edtPin2.requestFocus()
-                    }
-                }
-                R.id.edt_pin_4 -> {
-                    if (numberFour.length == 1) {
-                        includePinBinding.edtPin5.requestFocus()
-                    } else if (numberFour.isEmpty()) {
-                        includePinBinding.edtPin3.requestFocus()
-                    }
-                }
-                R.id.edt_pin_5 -> {
-                    if (numberFive.length == 1) {
-                        includePinBinding.edtPin6.requestFocus()
-                    } else if (numberFive.isEmpty()) {
-                        includePinBinding.edtPin4.requestFocus()
-                    }
-                }
-                R.id.edt_pin_6 -> {
-                    if (numberSix.isNotEmpty()) {
-                        val number = numberOne + numberTwo + numberThree + numberFour + numberFive + numberSix
-                        Toast.makeText(this@MainActivity, "Input : $number", Toast.LENGTH_LONG).show()
-                    } else {
-                        includePinBinding.edtPin5.requestFocus()
-                    }
-                }
-            }
+    private fun ActivityMainBinding.bindingApply() {
+        btnOk.setOnClickListener {
+            txtPin.text = ("Your Pin is ${pinEntry.number}")
         }
-
-        override fun afterTextChanged(p0: Editable?) { }
     }
 }
--- app/src/main/java/com/algokelvin/pinentryedittext/PinEntry.java
+++ app/src/main/java/com/algokelvin/pinentryedittext/PinEntry.java
@@ -1,4 +1,111 @@
 package com.algokelvin.pinentryedittext;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import com.algokelvin.pinentryedittext.databinding.IncludePinBinding;
+
 public class PinEntry {
+    private IncludePinBinding includePinBinding;
+    private String number;
+
+    public PinEntry(IncludePinBinding binding) {
+        this.includePinBinding = binding;
+        setTransformationMethod();
+        actionAddTextChangedListener();
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    private void setTransformationMethod() {
+        includePinBinding.edtPin1.setTransformationMethod(new AsteriskPasswordTransformationMethod());
+        includePinBinding.edtPin2.setTransformationMethod(new AsteriskPasswordTransformationMethod());
+        includePinBinding.edtPin3.setTransformationMethod(new AsteriskPasswordTransformationMethod());
+        includePinBinding.edtPin4.setTransformationMethod(new AsteriskPasswordTransformationMethod());
+        includePinBinding.edtPin5.setTransformationMethod(new AsteriskPasswordTransformationMethod());
+        includePinBinding.edtPin6.setTransformationMethod(new AsteriskPasswordTransformationMethod());
+    }
+
+    private void actionAddTextChangedListener() {
+        setAddTextChangedListener(includePinBinding.edtPin1);
+        setAddTextChangedListener(includePinBinding.edtPin2);
+        setAddTextChangedListener(includePinBinding.edtPin3);
+        setAddTextChangedListener(includePinBinding.edtPin4);
+        setAddTextChangedListener(includePinBinding.edtPin5);
+        setAddTextChangedListener(includePinBinding.edtPin6);
+    }
+
+    private void setAddTextChangedListener(EditText editText) {
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String numberOne = includePinBinding.edtPin1.getText().toString().trim();
+                String numberTwo = includePinBinding.edtPin2.getText().toString().trim();
+                String numberThree = includePinBinding.edtPin3.getText().toString().trim();
+                String numberFour = includePinBinding.edtPin4.getText().toString().trim();
+                String numberFive = includePinBinding.edtPin5.getText().toString().trim();
+                String numberSix = includePinBinding.edtPin6.getText().toString().trim();
+
+                switch (editText.getId()) {
+                    case R.id.edt_pin_1 : {
+                        if (numberOne.length() == 1) {
+                            includePinBinding.edtPin2.requestFocus();
+                        }
+                    }
+                    break;
+                    case R.id.edt_pin_2 : {
+                        if (numberTwo.length() == 1) {
+                            includePinBinding.edtPin3.requestFocus();
+                        } else if (numberTwo.isEmpty()) {
+                            includePinBinding.edtPin1.requestFocus();
+                        }
+                    }
+                    break;
+                    case R.id.edt_pin_3 : {
+                        if (numberThree.length() == 1) {
+                            includePinBinding.edtPin4.requestFocus();
+                        } else if (numberThree.isEmpty()) {
+                            includePinBinding.edtPin2.requestFocus();
+                        }
+                    }
+                    break;
+                    case R.id.edt_pin_4 : {
+                        if (numberFour.length() == 1) {
+                            includePinBinding.edtPin5.requestFocus();
+                        } else if (numberFour.isEmpty()) {
+                            includePinBinding.edtPin3.requestFocus();
+                        }
+                    }
+                    break;
+                    case R.id.edt_pin_5 : {
+                        if (numberFive.length() == 1) {
+                            includePinBinding.edtPin6.requestFocus();
+                        } else if (numberFive.isEmpty()) {
+                            includePinBinding.edtPin4.requestFocus();
+                        }
+                    }
+                    break;
+                    case R.id.edt_pin_6 : {
+                        if (!numberSix.isEmpty()) {
+                            number = numberOne + numberTwo + numberThree + numberFour + numberFive + numberSix;
+//                            Toast.makeText(this, "Input : $number", Toast.LENGTH_LONG).show()
+                        } else {
+                            includePinBinding.edtPin5.requestFocus();
+                        }
+                    }
+                    break;
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) { }
+        });
+    }
+
 }
