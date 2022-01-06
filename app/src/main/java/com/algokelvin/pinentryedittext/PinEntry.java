78740f9840a8840e761090cf5a0b9023cdefa989
algokelvin-373 <kelvinht373@gmail.com>
2022-01-06 11:20:09 +0700

    [1.0.0-alpha] Set Method digit pin

--- app/src/main/java/com/algokelvin/pinentryedittext/PinEntry.java
+++ app/src/main/java/com/algokelvin/pinentryedittext/PinEntry.java
@@ -10,6 +10,7 @@ public class PinEntry {
     private String number;
 
     public PinEntry(EditText[] edtTextPin) {
+        number = "";
         setEdtTextPin(edtTextPin);
         setTransformationMethod();
         actionAddTextChangedListener();
@@ -48,52 +49,18 @@ public class PinEntry {
                 }
 
                 switch (editText.getId()) {
-                    case R.id.edt_pin_1 : {
-                        if (digits[0].length() == 1) {
-                            edtTextPin[1].requestFocus();
-                        }
-                    }
-                    break;
-                    case R.id.edt_pin_2 : {
-                        if (digits[1].length() == 1) {
-                            edtTextPin[2].requestFocus();
-                        } else if (digits[1].isEmpty()) {
-                            edtTextPin[0].requestFocus();
-                        }
-                    }
-                    break;
-                    case R.id.edt_pin_3 : {
-                        if (digits[2].length() == 1) {
-                            edtTextPin[3].requestFocus();
-                        } else if (digits[2].isEmpty()) {
-                            edtTextPin[1].requestFocus();
-                        }
-                    }
-                    break;
-                    case R.id.edt_pin_4 : {
-                        if (digits[3].length() == 1) {
-                            edtTextPin[4].requestFocus();
-                        } else if (digits[3].isEmpty()) {
-                            edtTextPin[2].requestFocus();
-                        }
-                    }
-                    break;
-                    case R.id.edt_pin_5 : {
-                        if (digits[4].length() == 1) {
-                            edtTextPin[5].requestFocus();
-                        } else if (digits[4].isEmpty()) {
-                            edtTextPin[3].requestFocus();
-                        }
-                    }
-                    break;
-                    case R.id.edt_pin_6 : {
-                        if (!digits[5].isEmpty()) {
-                            number = digits[0] + digits[1] + digits[2] + digits[3] + digits[4] + digits[5];
-                        } else {
-                            edtTextPin[4].requestFocus();
-                        }
-                    }
-                    break;
+                    case R.id.edt_pin_1 : setFirstDigit(digits[0], edtTextPin[1]);
+                        break;
+                    case R.id.edt_pin_2 : setDigit(digits[1], edtTextPin[2], edtTextPin[0]);
+                        break;
+                    case R.id.edt_pin_3 : setDigit(digits[2], edtTextPin[3], edtTextPin[1]);
+                        break;
+                    case R.id.edt_pin_4 : setDigit(digits[3], edtTextPin[4], edtTextPin[2]);
+                        break;
+                    case R.id.edt_pin_5 : setDigit(digits[4], edtTextPin[5], edtTextPin[3]);
+                        break;
+                    case R.id.edt_pin_6 : setLastDigit(digits[5], edtTextPin[4]);
+                        break;
                 }
             }
 
@@ -101,6 +68,28 @@ public class PinEntry {
             public void afterTextChanged(Editable editable) { }
 
         });
+    }
+
+    private void setFirstDigit(String digit, EditText editText) {
+        if (digit.length() == 1) {
+            editText.requestFocus();
+        }
+    }
+
+    private void setDigit(String digit, EditText edtNext, EditText edtBefore) {
+        if (digit.length() == 1) {
+            edtNext.requestFocus();
+        } else if (digit.isEmpty()) {
+            edtBefore.requestFocus();
+        }
+    }
+
+    private void setLastDigit(String digit, EditText editText) {
+        if (!digit.isEmpty()) {
+            for (String s : digits) number += s;
+        } else {
+            editText.requestFocus();
+        }
     }
 
 }
