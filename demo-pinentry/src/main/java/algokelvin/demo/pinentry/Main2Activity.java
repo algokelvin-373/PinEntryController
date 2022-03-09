15b78dfc0e8f3deab678527399860604fe374474
algokelvin-373 <kelvinht373@gmail.com>
2022-03-09 23:30:19 +0700

    Success to pin with method PinEntry2

--- demo-pinentry/src/main/java/algokelvin/demo/pinentry/Main2Activity.java
+++ demo-pinentry/src/main/java/algokelvin/demo/pinentry/Main2Activity.java
@@ -1,7 +1,6 @@
 package algokelvin.demo.pinentry;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,9 +11,7 @@ import androidx.core.content.ContextCompat;
 public class Main2Activity extends AppCompatActivity {
     private TextView btnBack, btnAction;
     private boolean enBack = false;
-    private TextView[] edtTextPin;
-    private int dgt = 0;
-    private String pin = "";
+    private PinEntry2 pinEntry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,24 +28,12 @@ public class Main2Activity extends AppCompatActivity {
                 findViewById(R.id.edt_pin_5),
                 findViewById(R.id.edt_pin_6)
         };
-        setEdtTextPin(edtPin);
-        setTransformationMethod();
+        pinEntry = new PinEntry2(edtPin);
 
-    }
-
-    private void setEdtTextPin(TextView[] edtTextPin) {
-        this.edtTextPin = edtTextPin;
-    }
-
-    private void setTransformationMethod() {
-        for (TextView edt: edtTextPin) {
-            edt.setTransformationMethod(new AsteriskPasswordTransformationMethod());
-        }
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        Log.i("keyevent-down", String.valueOf(keyCode));
         switch (keyCode) {
             case 7:
             case 8:
@@ -72,15 +57,15 @@ public class Main2Activity extends AppCompatActivity {
     }
 
     private void textPassword(int num) {
-        if (dgt < edtTextPin.length && !enBack) {
-            edtTextPin[dgt++].setText(String.valueOf(num));
+        if (!enBack) {
+            pinEntry.textPassword(num);
             btnActionEnable();
         }
     }
 
     private void removePassword() {
-        if (dgt > 0 && !enBack) {
-            edtTextPin[--dgt].setText("");
+        if (!enBack) {
+            pinEntry.removePassword();
             btnActionEnable();
         }
     }
@@ -97,11 +82,11 @@ public class Main2Activity extends AppCompatActivity {
 
     private void btnActionController() {
         if (!enBack) {
-            if (dgt == edtTextPin.length) {
+            if (pinEntry.getDigits() == 6) {
                 StringBuilder dt = new StringBuilder();
-                for (TextView textView : edtTextPin)
+                for (TextView textView : pinEntry.getPin())
                     dt.append(textView.getText().toString());
-                pin = dt.toString();
+                String pin = dt.toString();
                 Toast.makeText(this, "Your PIN: "+ pin, Toast.LENGTH_LONG).show();
             }
         } else {
@@ -110,7 +95,7 @@ public class Main2Activity extends AppCompatActivity {
     }
 
     private void btnActionEnable() {
-        if (dgt == 6) enableBtnNext(R.drawable.bg_btn_action_active, R.color.white);
+        if (pinEntry.getDigits() == 6) enableBtnNext(R.drawable.bg_btn_action_active, R.color.white);
         else enableBtnNext(R.drawable.bg_btn_action_deactive, R.color.color_grey_9B9B9B);
     }
 
